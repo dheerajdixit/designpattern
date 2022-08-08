444fc4a216e9d7f85fe1aa3485aba37a884c0898
Dheeraj.Dixit <dheeraj.ndx@gmail.com>
2022-08-08 21:34:17 +0530

    design pattern -singleton

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using DP.Singleton;
+using System;
+namespace DesignPatterns
+{
+    class Prgoram
+    {
+        public static void Main()
+        {
+            //Singleton b;
+            var b = Singleton.Add2Plus2;
+            var c = Singleton.Instance;
+           
+            Console.WriteLine(b);
+            Console.ReadLine();
+        }
+    }
+}
